056a34bbb7fcbf09b043cba68dcb7a2447340b1a
David Der <xeldawe@gmail.com>
2025-02-13 02:03:18 +0100

    Load fix

--- load-app.sh
+++ load-app.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-echo "Running network.bat to create subnet..."
+echo "Running network.sh to create subnet..."
 ./network.sh
 
 # Load Docker images from .tar files in the /docker directory
-docker load -i docker/webapp_music-app.tar
-docker load -i docker/webapp_postgres.tar
-docker load -i docker/webapp_swagger-ui.tar
-docker load -i docker/webapp_redis.tar
-docker load -i docker/webapp_mysql.tar
-docker load -i docker/webapp_backend.tar
-docker load -i docker/webapp_frontend.tar
-docker load -i docker/webapp_nginx.tar
+docker load -i /docker/webapp_music-app.tar
+docker load -i /docker/webapp_postgres.tar
+docker load -i /docker/webapp_swagger-ui.tar
+docker load -i /docker/webapp_redis.tar
+docker load -i /docker/webapp_mysql.tar
+docker load -i /docker/webapp_backend.tar
+docker load -i /docker/webapp_frontend.tar
+docker load -i /docker/webapp_nginx.tar
 
 # Run Docker Compose
-docker-compose up --build
+docker-compose up 
